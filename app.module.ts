5e12f25a63af04af9d4358f0aa09f7e13c34a96d
meghachougule1199 <125789043+meghachougule1199@users.noreply.github.com>
2023-02-24 11:15:19 +0530

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,36 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BookigDetailsComponent } from './bookig-details/bookig-details.component';
+import { ViewBookingComponent } from './view-booking/view-booking.component';
+import { Router } from '@angular/router';
+import { ViewUserBookingComponent } from './view-user-booking/view-user-booking.component';
+//import { ViewBookingFEComponent } from './view-booking-fe/view-booking-fe.component';
+
+const allLinks:Routes=[
+  {path:'bookingdetails',component:BookigDetailsComponent},
+  {path:'viewbooking',component:ViewBookingComponent},
+ 
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BookigDetailsComponent,
+    ViewBookingComponent,
+    ViewUserBookingComponent
+
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(allLinks),FormsModule,CommonModule,HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
